350f0bbe2d1f7d678c4e6785ba5d64e5e652d036
“b1z3rr4” <natalia@b1z3rr4.com.br>
2024-05-06 21:47:54 -0300

    ajuste typescript

--- src/components/modules/Modal/index.tsx
+++ src/components/modules/Modal/index.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog'
+// @ts-ignore
 import { watchScreen } from 'metrics-analytics'
 import { ReactNode, useCallback } from 'react'
 import { CloseIcon, DialogClose, DialogContent, DialogOverlay } from './styles'
@@ -8,12 +9,12 @@ interface ModalProps {
 }
 
 export const Modal = ({ children }: ModalProps) => {
-
   const ref = useCallback((node: HTMLDivElement) => {
     if (node !== null) {
-      watchScreen(node, '1');
+      // @ts-ignore
+      watchScreen(node, '1')
     }
-  }, []);
+  }, [])
 
   return (
     <Dialog.Portal>
